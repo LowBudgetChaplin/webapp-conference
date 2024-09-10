8e144d39d551d524d47b53f900593b0d0ac23246
Serban Corodescu <serban.corodescu@totalsoft.ro>
2024-09-10 16:04:06 +0300

    commit after showing conferences + buttons

---
+++ src/features/conference/gql/fragments.js
@@ -0,0 +1,60 @@
+const { gql } = require('@apollo/client')
+
+const Fragments = {
+  conference: gql`
+    fragment conference on Conference {
+      id
+      name
+      startDate
+      endDate
+    }
+  `,
+
+  speaker: gql`
+    fragment speaker on Speaker {
+      id
+      name
+      isMainSpeaker
+    }
+  `,
+
+  location: gql`
+    fragment location on Location {
+      city {
+        id
+        name
+      }
+      county {
+        id
+        name
+      }
+      country {
+        id
+        name
+      }
+    }
+  `,
+
+  type: gql`
+    fragment type on Type {
+      id
+      name
+    }
+  `,
+
+  category: gql`
+    fragment category on Category {
+      id
+      name
+    }
+  `,
+
+  status: gql`
+    fragment status on Status {
+      id
+      name
+    }
+  `
+}
+
+export default Fragments
